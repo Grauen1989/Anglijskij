97cb2f5b32c57e8fb8ca7b3192feca5c787080af
Grauen1989 <gloom1989@mail.ru>
2021-12-02 22:39:31 +0300

    Povtorenie slov

---
+++ src/Random_words.java
@@ -0,0 +1,71 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+Что я хочу от программы?
+1) вводим в консоль число которое задаст количество слов из массива (или списка).
+2) программа поочередно выдает слова (без их повторения). Мы пишем их перевод. Если не верно - программа сама выдает перевод.
+3) программа выдает произношение слова
+ */
+
+public class Random_words {
+
+    public static void main (String [] args) {
+
+        ArrayList <String> words  = new ArrayList<String>();
+        words.add ("pain");
+        words.add ("socks");
+        words.add ("boots");
+        words.add ("lace");
+        words.add ("slippers");
+        words.add ("going to");
+        words.add ("inch");
+        words.add ("heel");
+        words.add ("tie");
+        words.add ("suit");
+        words.add ("wash");
+        words.add ("cotton");
+
+        ArrayList <String> perevod  = new ArrayList<String>();
+        words.add ("боль");
+        words.add ("носки");
+        words.add ("ботинки");
+        words.add ("шнурок");
+        words.add ("тапочки");
+        words.add ("собираться");
+        words.add ("дюйм");
+        words.add ("каблук");
+        words.add ("чай");
+        words.add ("костюм");
+        words.add ("мыть");
+        words.add ("хлопок");
+
+        int quantityWords = words.size();
+        System.out.println("Эта программа выводит случайные английские слова, начиная с 14.04.20. Слов в программе: " + quantityWords);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Сколько слов выдать?");
+        int c = in.nextInt ();
+        for (int i = 1; i<=c; i++)
+        {
+            String vvod = in.nextLine();
+            int randy = (int) (Math.random () * quantityWords);
+            System.out.println(words.get(randy));
+            System.out.println("Как переводится это слово?");
+          /* if (perevod.get(randy).equals(words.get(randy)))
+                System.out.println("Ты молодец");
+            else
+                System.out.println("Ты не молодец");*/
+        }
+        System.out.println(words);
+        int randy = (int) (Math.random () * quantityWords);
+        System.out.println(perevod.get(2));
+    /*    if (perevod.get(randy).equals(words.get(randy)))
+            System.out.println("Ты молодец");
+        else
+            System.out.println("Ты не молодец"); */
+    }
+
+}
